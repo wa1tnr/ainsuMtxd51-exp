2841f51edcb2d21ae38ce1350951634cc364f4db
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-15 23:29:54 +0000

    On branch testing-iiA-interpret-ab- 	modified:   src/dump.c

--- src/dump.c
+++ src/dump.c
@@ -10,29 +10,31 @@
 
 void say_something(void) { }
 
-// #ifdef NOT_DEFINED_NOW_BUT
+#define LBOUND 0x2040
+#define UBOUND  0x420
+
 /* dump 16 bytes of RAM in hex with ascii on the side */
 uint8_t* srdump(void) {
     char buffer[5] = "";
     strcpy(buffer, "SeVe") ;
     uint8_t *ram;
+
     // ram = (uint8_t *)7; // good - don't erase
 
-    // int p = 16397; // 16384 is 0x4000
-    // int p = 16197; // 16384 is 0x4000
-    int p = 16397; // 16384 is 0x4000
+    int p = LBOUND; // 16384 is 0x4000
 
     // ram = (uint8_t *)p; // ptr to address p
     // strcpy(buffer, (char *) p);
     // io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
 
-    for (int adrs = p; adrs < (p + 0x1000); adrs++) {
+    for (int adrs = p; adrs < (p + UBOUND); adrs++) {
         ram = (uint8_t *)adrs; // ptr to address
-        strcpy(buffer, (char *) adrs);
-        // int w = strtol(buffer);
-        // ram = w; // fake just to quiet compiler about unused 
-        io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
-
+        if (*ram > 127) io_write(io, (uint8_t *)".", 1);
+        if (*ram < 32)  io_write(io, (uint8_t *)".", 1);
+        if ((*ram > 31) && (*ram <128)) {
+            strcpy(buffer, (char *) adrs);
+            io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+        }
         for (volatile int j=2100; j>0; j--) { } // delay
     }
 
@@ -42,7 +44,6 @@ uint8_t* srdump(void) {
     // ram = (uint8_t *)7;
 
 //  ram = 7;
-//  blink_two();
  // int p = pop();
  // ram = (char*)p;
 
@@ -70,4 +71,50 @@ uint8_t* srdump(void) {
 }
 
 
-// #endif // #ifdef NOT_DEFINED_NOW_BUT
+
+/* dump 16 bytes of RAM in hex with ascii on the side */
+uint8_t* cdump(void) {
+    char buffer[5] = "";
+    char *ram;
+    // int p = pop();
+    int p = LBOUND ;
+    ram = (char*)p;
+
+ // sprintf(buffer, "%4x", p);
+    strcpy(buffer, (char *) p);
+
+
+
+    io_write(io, (uint8_t *)"\015\012", 2); // CRLF
+    io_write(io, (uint8_t *)"\015\012", 2); // CRLF
+    io_write(io, (uint8_t *)"\015\012", 2); // CRLF
+
+
+ // Serial.print(buffer);
+
+    io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+
+ // Serial.print("   ");
+
+    io_write(io, (uint8_t *)"  ", 3);
+
+    for (int i = 0; i < 16; i++) {
+        char c = *ram++;
+     // sprintf(buffer, " %2x", (c & 0xff));
+        strcpy(buffer, (char *) (c & 0xff));
+     // Serial.print(buffer);
+        io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+    }
+    ram = (char*)p;
+ // Serial.print("   ");
+    io_write(io, (uint8_t *)"  ", 3);
+    for (int i = 0; i < 16; i++) {
+        buffer[0] = *ram++;
+        if (buffer[0] > 0x7f || buffer[0] < ' ') buffer[0] = '.';
+        buffer[1] = '\0';
+     // Serial.print(buffer);
+        io_write(io, (uint8_t *)buffer, 1); // compiles cleanly
+    }
+ // push(p + 16);
+    return (uint8_t *)p;
+}
