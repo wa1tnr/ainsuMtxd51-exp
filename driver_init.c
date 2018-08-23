f66250f28bb0ea56652decbff824f7e31a2d15cf
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-23 17:05:07 +0000

    PA22, PA23 on SERCOM3 (from feather setups; this is metro)
    	modified:   driver_init.c
    
    On branch testing-iiA-interpret-ac-AUG-a-

--- driver_init.c
+++ driver_init.c
@@ -16,62 +16,78 @@ struct usart_sync_descriptor USART_0;
 void USART_0_PORT_init(void)
 {
 
-	gpio_set_pin_function(PB16, PINMUX_PB16C_SERCOM5_PAD0);
+/*
+3.0 is 22   3.1 is 23
 
-	gpio_set_pin_function(PB17, PINMUX_PB17C_SERCOM5_PAD1);
+#define  TX   GPIO(GPIO_PORTA, 22) // 3.0
+#define  PA22 GPIO(GPIO_PORTA, 22) // 3.0
+
+#define  RX   GPIO(GPIO_PORTA, 23) // 3.1
+#define  PA23 GPIO(GPIO_PORTA, 23) // 3.1
+*/
+
+   //   gpio_set_pin_function(PB16, PINMUX_PB16C_SERCOM5_PAD0); // TX
+	gpio_set_pin_function(PA22, PINMUX_PA22C_SERCOM3_PAD0); // TX
+
+   //   gpio_set_pin_function(PB17, PINMUX_PB17C_SERCOM5_PAD1); // RX
+	gpio_set_pin_function(PA23, PINMUX_PA23C_SERCOM3_PAD1); // RX
 }
 
 void USART_0_CLOCK_init(void)
 {
-	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_CORE, CONF_GCLK_SERCOM5_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
-	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM5_GCLK_ID_SLOW, CONF_GCLK_SERCOM5_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_CORE, CONF_GCLK_SERCOM3_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_SLOW, CONF_GCLK_SERCOM3_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
 
-	hri_mclk_set_APBDMASK_SERCOM5_bit(MCLK);
+    //  hri_mclk_set_APBDMASK_SERCOM3_bit(MCLK);
+        hri_mclk_set_APBBMASK_SERCOM3_bit(MCLK);
+
+/* hri_mclk_d51.h:2084:static inline void hri_mclk_set_APBBMASK_SERCOM3_bit(const void *const hw) */
 }
 
 void USART_0_init(void)
 {
 	USART_0_CLOCK_init();
-	usart_sync_init(&USART_0, SERCOM5, (void *)NULL);
+	usart_sync_init(&USART_0, SERCOM3, (void *)NULL);
 	USART_0_PORT_init();
 }
 
-void system_init(void)
-{
-	// init_mcu();  // local mod ainsu 19:04 UTC 13 Aug
+void system_init(void) {
 
-        // init_mcu() is problematic.
+/*
 
-        // Please see:
-        // wa1tnr/samd51_USART_basic/hpl/core/hpl_init.c
+  12:20 pm Philly cheese steak 16"
 
-	// GPIO on PA21
+*/
+        // init_mcu();
 
-	// Set pin direction to output
-	gpio_set_pin_direction(D11, GPIO_DIRECTION_OUT);
+	// gpio_set_pin_direction(D11, GPIO_DIRECTION_OUT);
 
-	gpio_set_pin_level(D11,
-	                   // <y> Initial level
-	                   // <id> pad_initial_level
-	                   // <false"> Low
-	                   // <true"> High
-	                   false);
+	// gpio_set_pin_level(D11, false);
 
-	gpio_set_pin_function(D11, GPIO_PIN_FUNCTION_OFF);
+	// gpio_set_pin_function(D11, GPIO_PIN_FUNCTION_OFF);
 
-	// GPIO on PA23
-
-	// Set pin direction to output
 	gpio_set_pin_direction(D13, GPIO_DIRECTION_OUT);
 
-	gpio_set_pin_level(D13,
-	                   // <y> Initial level
-	                   // <id> pad_initial_level
-	                   // <false"> Low
-	                   // <true"> High
-	                   false);
+	gpio_set_pin_level(D13, false);
 
-	gpio_set_pin_function(D13, GPIO_PIN_FUNCTION_OFF);
+	// gpio_set_pin_function(D13, GPIO_PIN_FUNCTION_OFF);
 
 	USART_0_init();
 }
+
+
+/* want D11, D13 */
+/*
+13 16 11 19
+
+  D13 PA16   D11 PA19
+*/
+
+                                    /* hri_mclk_set_APBDMASK_SERCOM3_bit(MCLK); */
+
+/*
+ rvim $ ag  hri_mclk | egrep SERC |  egrep MASK | egrep SERCOM3
+hri_mclk_d51.h:2084:static inline void hri_mclk_set_APBBMASK_SERCOM3_bit(const void *const hw)
+
+
+*/
