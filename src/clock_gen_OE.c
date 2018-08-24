28728b6242880c1523f724a032a7074608149083
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-24 20:21:31 +0000

    comments documenting pin selection

--- src/clock_gen_OE.c
+++ src/clock_gen_OE.c
@@ -5,6 +5,33 @@
 // 24 August SAMD51 Metro M4 Express
 void clock_gen_oe(void) { // ainsu new idiom 24 August 2018
 #ifndef OMIT_GCLK0_OE_PINMUX_SETUP
+
+    // Possible pins for SAMD21, for this output, are:
+
+    // PA14  PA27  PA28  PA30
+    // PB14  PB22
+
+    // Pin D4 (which is also PA14) on SAMD51 (Feather M4 Express)
+    // is known to work, also.
+
+    // PA14 (board D2) Metro M0 Express, only // ainsuforth wa1tnr TODO
+
+    // configure PA14 (board D2) as GCLK_IO[0] output of main clock generator,
+    // to confirm 48 MHz operation
+    // set pin as output
+
+    // SAMD51
+
+    // Metro M4 Express:
+
+    //     PA14 MISO
+    //     PA30 SWCLK
+    //     PB14 D5
+    //     PB22 NEOPIX
+
+    // Clock generation output can be configured on the mux for any
+    // of those four pins.  D5 is a good candidate.
+
 #if defined ADAFRUIT_METRO_M4_EXPRESS
     PORT->Group[PORTB].DIRSET.reg  = (uint32_t)(1 << 14); // PB14 //  1  5 pinmode  // D5 // SAMD51
 
