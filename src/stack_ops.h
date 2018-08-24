c303b845d8a41d9579cdcd477b72840dfdfb2c8e
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-24 23:59:30 +0000

    stack operations
    	new file:   stack_ops.h
    
    On branch xKM_converser_d51-bb-

---
+++ src/stack_ops.h
@@ -0,0 +1,12 @@
+/* stack_ops.h */
+
+// const int STKSIZE = 8;
+// const int STKMASK = 7;
+extern int stack[];
+extern int p = 0;
+
+/* TOS is Top Of Stack */
+// #define TOS stack[p]
+
+extern void push(int n);
+extern int pop(void);
