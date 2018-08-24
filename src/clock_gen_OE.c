d780cc220e764fa7b411a3bd5d73f5d33f7089b2
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-24 20:13:44 +0000

    GCLK_IO0 OE - monitor CPU freq on PB14 - D5

--- src/clock_gen_OE.c
+++ src/clock_gen_OE.c
@@ -1,4 +1,15 @@
-// 24 August SAMD51 Metro M4 Express
-
+#include <atmel_start.h>
+#include "pins.h"
+#include "clock_gen_OE.h"
 void clock_gen_oe(void) { // ainsu new idiom 24 August 2018
+#if defined ADAFRUIT_METRO_M4_EXPRESS
+    PORT->Group[PORTB].DIRSET.reg  = (uint32_t)(1 << 14); // PB14 //  1  5 pinmode  // D5 // SAMD51
+
+    PORT->Group[PORTB].PINCFG[14].bit.PMUXEN = 1; // enable the peripheral mux for this pin
+
+    PORT->Group[PORTB].PMUX[(14>>1)].bit.PMUXE
+        = MUX_PB14M_GCLK_IO0; // select the GCLK_IO0 peripheral function
+
+    GCLK->GENCTRL[0].bit.OE = 1; // enable output from clock generator 0
+#endif
 }
