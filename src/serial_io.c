34325d1c635a6bb27389051eb02c19b1e62a4f33
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-24 21:05:36 +0000

    On branch master
    A single commit on branch master.  To merge over
    to the development branch in a moment. ;)
    	modified:   src/serial_io.c

--- src/serial_io.c
+++ src/serial_io.c
@@ -154,7 +154,8 @@ void USART_0_example_lower(void) {
     //  "version: On branch ainsuMtxd51-exp-cc-\r\n",                40);
     //  "timestamp:  Thu Aug 23 19:02:39 UTC 2018\r\n",              42);
     io_write(io, (uint8_t *)
-        "xKM_converser_d51-bb-    ", 25); // private branch - see KM_converser_d51 instead
+        "On branch master         ", 25); // reflect that this is really branch master
+     // "xKM_converser_d51-bb-    ", 25); // private branch - see KM_converser_d51 instead
 
 /*
     io_write(io, (uint8_t *)
@@ -162,7 +163,7 @@ void USART_0_example_lower(void) {
 */
 
     io_write(io, (uint8_t *)
-        "Fri Aug 24 20:46:58 UTC 2018\r\n\r\n",                      32);
+        "Fri Aug 24 21:02:28 UTC 2018\r\n\r\n",                      32);
     /*   12345678901234567890123456789012345678901234567890
                  10        20        30        40        50 */
 
