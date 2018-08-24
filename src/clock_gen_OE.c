e8fb9fe11b979eec38d381d4d66854afef0361bd
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-24 16:52:09 +0000

    Enable output from clock generator 0
    	new file:   clock_gen_OE.c
    	new file:   clock_gen_OE.h
    
    On branch xKM_converser_d51-aa-

---
+++ src/clock_gen_OE.c
@@ -0,0 +1,4 @@
+// 24 August SAMD51 Metro M4 Express
+
+void clock_gen_oe(void) { // ainsu new idiom 24 August 2018
+}
---
+++ src/clock_gen_OE.h
@@ -0,0 +1,1 @@
+extern void clock_gen_oe(void); // { // ainsu new idiom 24 August 2018
