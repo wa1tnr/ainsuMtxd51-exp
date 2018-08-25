65f2d017f3153995d22621ddc32a50a836e16b88
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-25 20:15:18 +0000

    prelude_to_cleo() complete.
    variables added to other functions.
    
    commented function added (static void,
    so has to be a comment, here).
    
    On branch xKM_converser_d51-bb-
    	modified:   itoa.c

--- src/itoa.c
+++ src/itoa.c
@@ -10,18 +10,35 @@
   Copyright (c) 2014 Arduino LLC.  All right reserved.
 */
 
+#include <atmel_start.h>
+#include "serial_io.h"
+
 char pr_strn[63]; // print string - weak naming.  review. TODO wa1tnr
 int input_intgr = 0; // input integer - also a weak name. ;)  TODO wa1tnr
 
 void prelude_to_cleo(void) {
+    for (int jk = 7; jk > 0; jk--) {
+        io_write(io, (uint8_t *)"m", 1);
+    }
 }
 
 void call_me_now_ms_cleo(void) { // call me now for your free reading
     prelude_to_cleo(); // test basic io_write() functionality
 }
 
+/* reverse:  reverse string s in place */
+/*
+static void reverse(char s[]) {
+  int i, j ;
+  char c ;
+}
+*/
+
 /* itoa:  convert n to characters in s */
 extern void itoa( int n, char s[] ) {
+/*
+    int i, sign ;
+*/
 }
 
 /*
