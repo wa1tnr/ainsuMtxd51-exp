561cfe8b7c9ec2252d1f3110cd9a3c6e9d1fa50e
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-25 22:56:52 +0000

    code cleanup 	modified:   warm.h On branch xKM_converser_d51-cc-

--- src/warm.h
+++ src/warm.h
@@ -1,15 +1,3 @@
-/* warm.h */
-/* wa1tnr - August, 2018 LGPL v2.1 */
-
-// #include "driver_examples.h"
-// #include "driver_init.h"
-// #include "utils.h"
-
-/* Terminal Input Buffer for interpreter */
-// #define maxtib 16
-
-// extern struct io_descriptor *io;
 extern int state_warm; // = 0;
-extern void _warm(void); // { // reboot the machine
-extern void capture_warm(); // { // trap keystrokes to build a command
-
+extern void _warm(void); // reboot the machine
+extern void capture_warm(void); // trap keystrokes to build a command
