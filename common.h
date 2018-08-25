f835459b759fe83f0eca07dbc77b3fe4e8bf610f
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-25 22:55:50 +0000

    See source. ;)
    	modified:   ../common.h
    
    On branch xKM_converser_d51-cc-

--- common.h
+++ common.h
@@ -1,4 +1,7 @@
 /* common.h */
-#include "atmel_start.h" // types
+// types
+#include "atmel_start.h"
 #define maxtib 16
+// the stack needs a buffer for ASCII strings - the dot word has this
+#define stackbuflen 64
 extern uint8_t *buf;
