5dafd4d7f204172068eacfc450aebc95ba762548
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-25 23:02:16 +0000

    The dot word.  In full form.
    	modified:   stack_ops.c
    
    On branch xKM_converser_d51-cc-

--- src/stack_ops.c
+++ src/stack_ops.c
@@ -10,6 +10,11 @@
    so overflow and underflow are not possible
    Number of items must be a power of 2 */
 
+#include <atmel_start.h>
+#include "common.h"
+#include "itoa.h"
+#include "serial_io.h"
+#include <string.h>
 
 // const int STKSIZE = 8;
 #define STKSIZE 8
@@ -38,6 +43,14 @@ int pop(void) {
 
 /* destructively display top of stack, decimal */
 void dot(void) {
-    // Serial.print(pop());
-    // Serial.print(" ");
+    _spc(); _spc(); // print formatting/spacing
+    // push(7345280); // could do this here as a test
+    int len_pr_strn = 0;
+    int popped = pop();
+    pr_strn[0] = (uint32_t) "\0";
+    input_intgr = popped; // 5280; // feet in a mile
+    itoa (input_intgr, pr_strn);
+    len_pr_strn = strlen(pr_strn);
+    io_write(io, (uint8_t *)pr_strn, len_pr_strn);
+    _spc();
 }
