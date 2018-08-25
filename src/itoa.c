03f425e9dc81076aedd20037436ac6db64cc1d37
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-25 19:51:11 +0000

    LICENSE LGPL v2.1 from Arduino LLC code base.
    	modified:   itoa.c
    
    On branch xKM_converser_d51-bb-

--- src/itoa.c
+++ src/itoa.c
@@ -1,1 +1,35 @@
 // Sat Aug 25 19:30:14 UTC 2018
+
+/*
+  LICENSE: LGPL v2.1 Arduino LLC > Adafruit.com > you and me ;)
+
+  adopted for ainsMtxd51 by wa1tnr 25 August 2018
+
+  .arduino15/packages/adafruit/hardware/samd/1.2.1/cores/arduino/itoa.c
+
+  Copyright (c) 2014 Arduino LLC.  All right reserved.
+*/
+
+
+// program code here
+
+
+/*
+  .arduino15/packages/adafruit/hardware/samd/1.2.1/cores/arduino/itoa.c
+
+  Copyright (c) 2014 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
