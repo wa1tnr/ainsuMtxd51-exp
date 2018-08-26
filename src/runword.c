04152f783b3bd1d7a7810b2d1237610789a257eb
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-26 21:44:53 +0000

    io_write report added.
    On branch xKM_converser_d51-ee-fixed-
    
    	modified:   runword.c

--- src/runword.c
+++ src/runword.c
@@ -1,12 +1,16 @@
-// Sun Aug 26 20:05:01 UTC 2018
+// Sun Aug 26 21:43:30 UTC 2018
 
 /* runword.c */
 
+#include <atmel_start.h>
 #include "dict.h"
+#include "serial_io.h"
 
 void runword(void) {
     int place = locate();
+    io_write(io,     (uint8_t *)" ~runword place echo~ ", 22);
     if ((place != 0) & (place < (entries - 1))) {
         return;
     }
+    return;
 }
