2d3288ba5ca3ffad9d09b4670fa5d18d6218d491
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-26 20:24:31 +0000

    flash dictionary primitives
    Forth Dictionary sub-project
    
    NAMED creates a string in flash
    
    On branch xKM_converser_d51-ee-fixed-
    
    	modified:   dict.c

--- src/dict.c
+++ src/dict.c
@@ -1,1 +1,22 @@
 // Sun Aug 26 19:45:14 UTC 2018
+
+/* dict.c */
+
+/* NAMED creates a string in flash */
+#define NAMED(x, y) const char x[]=y
+
+NAMED(_dott, ".");
+void dott(void) { // earlier dot() word bypassed temporarily
+    // io_write(io, (uint8_t *)" ~dot~", 7);
+}
+
+NAMED(_nopp, "nop");
+void nopp() { }
+
+NAMED(_nop, " ");
+void nop() { }
+
+NAMED(_swap, "swap");
+void swap() {
+    // io_write(io, (uint8_t *)" ~swap~", 7);
+}
