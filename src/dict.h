a991e7169eaf951f79873e5f7e08b739b7ec0344
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-26 20:36:50 +0000

    flash dictionary primitives
    Forth Dictionary sub-project
    
    typedef func, typedef struct {} entry
    
    On branch xKM_converser_d51-ee-fixed-
    
    	modified:   dict.h

--- src/dict.h
+++ src/dict.h
@@ -1,1 +1,10 @@
 // Sun Aug 26 19:45:14 UTC 2018
+
+/* dict.h */
+
+typedef void (*func)(void);           // signature of functions in dictionary
+
+typedef struct {
+    const char*    name;              // Pointer the Word Name in flash
+    const func     function;          // Pointer to function
+} entry;
