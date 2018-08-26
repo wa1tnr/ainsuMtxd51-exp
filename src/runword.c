71eb2b224fab6dc08ee05ee60d1117fc13fd091c
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-26 21:52:37 +0000

    runword() mods
       include "stack_ops.h"
    
       io_write x2 added to help debug program flow
    
       push() and dot() leveraged inside the program here
    
    On branch xKM_converser_d51-ee-fixed-
    
    	modified:   runword.c

--- src/runword.c
+++ src/runword.c
@@ -5,11 +5,17 @@
 #include <atmel_start.h>
 #include "dict.h"
 #include "serial_io.h"
+#include "stack_ops.h"
 
 void runword(void) {
     int place = locate();
     io_write(io,     (uint8_t *)" ~runword place echo~ ", 22);
+    push(place);
+    io_write(io, (uint8_t *)    " ~runword dot~ ", 15);
+                             /*  1234567890123456789012345678901234567890 */
+    dot();
     if ((place != 0) & (place < (entries - 1))) {
+        io_write(io, (uint8_t *)" ~runword place~ ", 17);
         return;
     }
     return;
