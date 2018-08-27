81369a3679ca8544fcbf140999fd801898f7be33
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-27 19:42:35 +0000

    Atmel Start - 27 Aug 19:17 UTC

--- driver_init.c
+++ driver_init.c
@@ -16,20 +16,8 @@ struct usart_sync_descriptor USART_0;
 void USART_0_PORT_init(void)
 {
 
-/*
-3.0 is 22   3.1 is 23
-
-#define  TX   GPIO(GPIO_PORTA, 22) // 3.0
-#define  PA22 GPIO(GPIO_PORTA, 22) // 3.0
-
-#define  RX   GPIO(GPIO_PORTA, 23) // 3.1
-#define  PA23 GPIO(GPIO_PORTA, 23) // 3.1
-*/
-
-   //   gpio_set_pin_function(PB16, PINMUX_PB16C_SERCOM5_PAD0); // TX
 	gpio_set_pin_function(PA22, PINMUX_PA22C_SERCOM3_PAD0); // TX
 
-   //   gpio_set_pin_function(PB17, PINMUX_PB17C_SERCOM5_PAD1); // RX
 	gpio_set_pin_function(PA23, PINMUX_PA23C_SERCOM3_PAD1); // RX
 }
 
@@ -38,10 +26,7 @@ void USART_0_CLOCK_init(void)
 	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_CORE, CONF_GCLK_SERCOM3_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
 	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_SLOW, CONF_GCLK_SERCOM3_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
 
-    //  hri_mclk_set_APBDMASK_SERCOM3_bit(MCLK);
-        hri_mclk_set_APBBMASK_SERCOM3_bit(MCLK);
-
-/* hri_mclk_d51.h:2084:static inline void hri_mclk_set_APBBMASK_SERCOM3_bit(const void *const hw) */
+	hri_mclk_set_APBBMASK_SERCOM3_bit(MCLK);
 }
 
 void USART_0_init(void)
@@ -51,43 +36,51 @@ void USART_0_init(void)
 	USART_0_PORT_init();
 }
 
-void system_init(void) {
+void system_init(void)
+{
+	// init_mcu();  TODO wa1tnr 27 Aug: why does this inhibit 'things'? (everything iirc)
 
-/*
+	// GPIO on PA16
 
-  12:20 pm Philly cheese steak 16"
+	// Set pin direction to output
+	gpio_set_pin_direction(PA16, GPIO_DIRECTION_OUT);
 
-*/
-        // init_mcu();
+	gpio_set_pin_level(PA16,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
 
-	// gpio_set_pin_direction(D11, GPIO_DIRECTION_OUT);
+	gpio_set_pin_function(PA16, GPIO_PIN_FUNCTION_OFF);
 
-	// gpio_set_pin_level(D11, false);
+	// GPIO on PA19
 
-	// gpio_set_pin_function(D11, GPIO_PIN_FUNCTION_OFF);
+	// Set pin direction to output
+	gpio_set_pin_direction(PA19, GPIO_DIRECTION_OUT);
 
-	gpio_set_pin_direction(D13, GPIO_DIRECTION_OUT);
+	gpio_set_pin_level(PA19,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
 
-	gpio_set_pin_level(D13, false);
+	gpio_set_pin_function(PA19, GPIO_PIN_FUNCTION_OFF);
 
-	// gpio_set_pin_function(D13, GPIO_PIN_FUNCTION_OFF);
+	// GPIO on PB14
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PB14, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_level(PB14,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	gpio_set_pin_function(PB14, GPIO_PIN_FUNCTION_OFF);
 
 	USART_0_init();
 }
-
-
-/* want D11, D13 */
-/*
-13 16 11 19
-
-  D13 PA16   D11 PA19
-*/
-
-                                    /* hri_mclk_set_APBDMASK_SERCOM3_bit(MCLK); */
-
-/*
- rvim $ ag  hri_mclk | egrep SERC |  egrep MASK | egrep SERCOM3
-hri_mclk_d51.h:2084:static inline void hri_mclk_set_APBBMASK_SERCOM3_bit(const void *const hw)
-
-
-*/
