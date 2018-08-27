b3e9cb9f37036815e196ab3937b9db2eb61b65a9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-27 04:03:21 +0000

    modified:   serial_io.c

--- src/serial_io.c
+++ src/serial_io.c
@@ -166,9 +166,9 @@ void USART_0_example_lower(void) {
         "UART pins TX and RX used with CP2104 bridge.\r\n\r\n",      48);
 
     io_write(io, (uint8_t *)
-        "xKM_converser_d51-gg-    ", 25); // private branch - see KM_converser_d51 instead
+     // "xKM_converser_d51-gg-    ", 25); // private branch - see KM_converser_d51 instead
      //  12345678901234567890123456789012345678901234567890
-     // "KM_converser_d51         ", 25);
+        "KM_converser_d51         ", 25);
 
      // prep for merge into master, 25 Aug 20:58 UTC:
      // "On branch master         ", 25); // reflect that this is really branch master
@@ -179,7 +179,7 @@ void USART_0_example_lower(void) {
 */
 
     io_write(io, (uint8_t *)
-        "Mon Aug 27 03:11:00 UTC 2018\r\n\r\n",                      32);
+        "Mon Aug 27 04:01:29 UTC 2018\r\n\r\n",                      32);
     /*   12345678901234567890123456789012345678901234567890
                  10        20        30        40        50 */
 
