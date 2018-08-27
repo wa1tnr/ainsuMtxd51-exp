9b4b90e0c96061a8eb2993070d60b7d2ab6f33de
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-27 00:57:26 +0000

    On branch xKM_converser_d51-ee-fixed-
    Console print for KM_converser_d51 changed over.
    
    Never edit in KM_converser branch directly; it
    only is used to gain new merges (which are then
    merged up to master, if desired).
    
    	modified:   serial_io.c

--- src/serial_io.c
+++ src/serial_io.c
@@ -168,9 +168,9 @@ void USART_0_example_lower(void) {
     //  "version: On branch ainsuMtxd51-exp-cc-\r\n",                40);
     //  "timestamp:  Thu Aug 23 19:02:39 UTC 2018\r\n",              42);
     io_write(io, (uint8_t *)
-        "xKM_converser_d51-ee-fixed-    ", 31); // private branch - see KM_converser_d51 instead
+     // "xKM_converser_d51-ee-fixed-    ", 31); // private branch - see KM_converser_d51 instead
      //  12345678901234567890123456789012345678901234567890
-     // "KM_converser_d51         ", 25); // private branch - see KM_converser_d51 instead
+        "KM_converser_d51         ", 25); // private branch - see KM_converser_d51 instead
 
      // prep for merge into master, 25 Aug 20:58 UTC:
      // "On branch master         ", 25); // reflect that this is really branch master
