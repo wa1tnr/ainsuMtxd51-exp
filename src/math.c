0e1d3a22761cb8bb9118cd1b5d478a68625e932c
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-27 04:34:09 +0000

    The two definitions. On branch xKM_converser_d51-gg- 	modified:   math.c

--- src/math.c
+++ src/math.c
@@ -1,1 +1,20 @@
 /* math.c */
+
+#include "stack_ops.h"
+
+/* add top two stack items */
+// NAMED(_plus, "plus");
+void plus(void) {
+    int a = 0;
+    int b = 0;
+    a = pop();
+    b = pop();
+    push(a+b);
+}
+
+/* add top two items */
+// NAMED(_add, "+");
+void add(void) {
+    int a = pop();
+    TOS = a + TOS;
+}
