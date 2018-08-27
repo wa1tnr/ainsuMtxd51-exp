a566db92b8ce0077294268f161ba69444d2f4a34
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-27 19:09:49 +0000

    extern int crlfstate declared

--- src/readword.h
+++ src/readword.h
@@ -1,3 +1,10 @@
+extern int crlfstate;
 extern void readword(void);
 extern int isNumber(void);
 extern int number();
+// extern uint8_t pos;
+// extern void printing(void);
+
+extern void throw_(void);
+
+// extern uint8_t reading(void);
