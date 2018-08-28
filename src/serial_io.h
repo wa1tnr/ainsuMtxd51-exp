d60c267105509abae957daf7698af17e76b8edd1
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-28 06:09:10 +0000

    extern for cr()

--- src/serial_io.h
+++ src/serial_io.h
@@ -12,6 +12,7 @@ extern void _ok(void);
 extern void _bkbar(void); // broken bar
 extern void _spc(void);
 extern void _cr(void); // line 113
+extern void cr(void);
 extern void USART_0_example_upper(void);
 extern void USART_0_example_lower(void);
 
