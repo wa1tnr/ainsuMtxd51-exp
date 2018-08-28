74dcecf3b12fc80a7625539bba9e3f8f8920f846
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-28 06:13:37 +0000

    Many trials.
    strlen() was reliable as a test, as the length of
    the empty input line is unique.
    
    Wanted a good 'ok' prompt response to an empty input line.
    
    On branch xKM_converser_d51-kk-
    	modified:   runword.c

--- src/runword.c
+++ src/runword.c
@@ -3,11 +3,17 @@
 /* runword.c */
 
 #include <atmel_start.h>
+#include <string.h>
+#include "common.h"
 #include "dict.h"
 #include "serial_io.h"
 #include "stack_ops.h"
 #include "readword.h"
 #include "debugging.h"
+#include "getKey.h"
+
+char spcbuf[2];
+char crbuf[2];
 
 void runword(void) {
     int place = locate();
@@ -19,10 +25,12 @@ void runword(void) {
                              /*  1234567890123456789012345678901234567890 */
     }
     if ( DEBUG_FORTH_DICT_PRIMITIVES ) {
+        io_write(io, (uint8_t *)    " ~Place~ ", 9);
         push(place); dot();
     }
     // 5 #define LOCATE_BOUNDS_ERR -11
-    if ((place != LOCATE_BOUNDS_ERR) & (place < (entries - 1))) { // if ((place != 0) & ..
+    if ((place != LOCATE_BOUNDS_ERR) // 0
+        & (place < (entries - 1))) { // if ((place != -11) & ..
         if ( DEBUG_FORTH_DICT_PRIMITIVES ) {
             io_write(io, (uint8_t *)" ~runword place fgh~ ", 20);
         }
@@ -42,5 +50,31 @@ void runword(void) {
         _ok();
         return;
     }
+
+    if (strlen(tib) == 0) { // reliable test for no input on a cr
+        // io_write(io, (uint8_t *)"  strlen(tib) == 0 ", 19);
+        _ok(); return;
+    }
+
+/*
+    strcpy(crbuf, (char *)'\015');
+
+    if (strcmp(tib, crbuf)) { io_write(io, (uint8_t *)" ~strcmp compared~ ", 19); _ok(); return; }
+
+    if (crlfstate == -1) { io_write(io, (uint8_t *)" ~crlfstate is -1~ ", 19); _ok(); return; }
+
+    if (crlfstate == 0) { io_write(io, (uint8_t *)" ~crlfstate is  0~ ", 19); _que(); return; }
+
+    // strcpy(spcbuf, " ");
+
+    // strcpy(crbuf, "\015\0");
+
+    // if (!strcmp(tib, crbuf)) { _ok(); return; }
+
+    // if (strcmp(tib, spcbuf)) { nop(); return; }
+
+    // if (crlfstate == -1) { _cr(); _cr(); _cr(); }
+
+*/
     _que(); // Serial.println("?");
 }
