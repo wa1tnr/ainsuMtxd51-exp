d3f2fd26c0e48476df32327ef87081cbb00a3dd3
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-28 19:59:13 +0000

    NAMED _dump _words _warm_
    On branch xKM_converser_d51-kk-
    	modified:   src/flashDict.h

--- src/flashDict.h
+++ src/flashDict.h
@@ -1,9 +1,12 @@
 /* flashDict.h */
 #include "dict_common.h"
-NAMED(_drop, "drop");
-NAMED(_back, "back");
-NAMED(_swap, "swap");
-NAMED(_dotS, ".s");
-NAMED(_plus, "plus");
-NAMED(_add,  "+");
-NAMED(_cr_n, "cr");
+NAMED(_drop,  "drop");
+NAMED(_back,  "back");
+NAMED(_swap,  "swap");
+NAMED(_dotS,  ".s");
+NAMED(_plus,  "plus");
+NAMED(_add,   "+");
+NAMED(_cr_n,  "cr");
+NAMED(_dump,  "dump");
+NAMED(_words, "words");
+NAMED(_warm_, "warm");
