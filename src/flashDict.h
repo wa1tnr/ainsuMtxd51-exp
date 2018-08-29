0119c5dab320b07b90b9540e020be3c7b93b56b9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-29 02:45:04 +0000

    NAMED macros
      "dup"  duplicate
      "*"    multiply
      "-"    subtract
    
    On branch xKM_converser_d51-nn-
    
    	modified:   flashDict.h

--- src/flashDict.h
+++ src/flashDict.h
@@ -1,11 +1,14 @@
 /* flashDict.h */
 #include "dict_common.h"
 NAMED(_drop,  "drop");
+NAMED(_dup,   "dup");
 NAMED(_back,  "back");
 NAMED(_swap,  "swap");
 NAMED(_dotS,  ".s");
 NAMED(_plus,  "plus");
 NAMED(_add,   "+");
+NAMED(_multiply, "*");
+NAMED(_subtract, "-");
 NAMED(_cr_n,  "cr");
 NAMED(_dump,  "dump");
 NAMED(_words, "words");
