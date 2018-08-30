4caea90d766b961e9c85a14c11f14fdebd4bf6a0
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-30 23:55:14 +0000

    km conv

--- src/serial_io.c
+++ src/serial_io.c
@@ -1,4 +1,4 @@
-// Thu Aug 30 23:13:28 UTC 2018
+// Thu Aug 30 23:54:19 UTC 2018
 // On branch xKM_converser_d51-pp-
 
 /* wa1tnr - August, 2018 LGPL v2.1 */
@@ -21,7 +21,7 @@
 #include "debugging.h"
 #include "dict_common.h"
 
-#define timeStamp(t,l)   "Thu Aug 30 23:13:28 UTC 2018\r\n\r\n", 32
+#define timeStamp(t,l)   "Thu Aug 30 23:54:19 UTC 2018\r\n\r\n", 32
 
 /*
 #define branchStamp(b,l) "xKM_converser_d51-pp-    ", 25
