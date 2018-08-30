b580f6fbc970d2ffe4872ffd625eac8c6900e841
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-30 23:17:41 +0000

    rev_coll_stack() forth C macro - factored somewhat

--- src/fmacros.h
+++ src/fmacros.h
@@ -9,3 +9,48 @@
     for (int i = 8; i > 0; i--) { \
         uBackDotBack() \
     }
+
+#define bk_bk_dr() \
+    back(); \
+    back(); \
+    drop();
+
+#define swdr() \
+    swap(); \
+    drop();
+
+#define swdr_four() \
+    for (int mi = 4; mi > 0; mi--) { \
+        swap(); drop();  \
+    }
+
+#define drp_four() \
+    for (int mj = 4; mj > 0; mj--) { \
+        drop();  \
+    }
+
+#define swdr_three() \
+    for (int mk = 3; mk > 0; mk--) { \
+        swap(); drop();  \
+    }
+
+#define bkbkdr_three() \
+    for (int mk = 3; mk > 0; mk--) { \
+        bk_bk_dr(); \
+    }
+
+#define rev_coll_stack() \
+    swdr_four();    \
+    swdr_four();    \
+    drop();         \
+    swdr_four()     \
+    swap();         \
+    drp_four()      \
+    swdr_four()     \
+    drp_four()      \
+    swdr_three()    \
+    bkbkdr_three()  \
+    swdr();         \
+    swap();         \
+    bk_bk_dr();     \
+    swap();
