eccea97460ab21ddf245d92fac0458333662e2d9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-31 21:21:51 +0000

    Storage of programs for this forth-like environment.
    On branch zKM_converser_d51-aa-
    	new file:   programs.c

---
+++ src/programs.c
@@ -0,0 +1,18 @@
+// Thu Aug 30 20:51:14 UTC 2018
+// On branch xKM_converser_d51-pp-
+
+/* programs.c */
+
+// #include <atmel_start.h>
+// #include "common.h"
+// #include "itoa.h"
+// #include "serial_io.h"
+// #include <string.h>
+// #include "stack_ops.h"
+#include "programs.h"
+#include "fmacros.h"
+
+void prog_a(void) {
+    fmac_prog_a();
+}
+
