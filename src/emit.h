6e87a72a280354d4c32e94fd9b90e6f5883bd3ce
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-31 03:10:17 +0000

    includes and a prototype for emit()

--- src/emit.h
+++ src/emit.h
@@ -1,1 +1,11 @@
+// Fri Aug 31 02:48:02 UTC 2018
+// On branch xKM_converser_d51-pp-
+
 /* emit.h */
+
+#include "stack_ops.h" // pop()
+#include "atmel_start.h" // io_write
+#include "serial_io.h" // io itself
+#include "fmacros.h" // forth macros
+
+extern void emit(void);
