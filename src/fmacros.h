9e155fb4162f9e458201cb09bd22832a7f97a19f
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-31 21:18:47 +0000

    A working forth-alike program written in the C preprocessor macro facility.
    On branch zKM_converser_d51-aa-
    
    	modified:   fmacros.h

--- src/fmacros.h
+++ src/fmacros.h
@@ -78,4 +78,80 @@
     char_r = (int)dfbyte; \
     io_write(io, (uint8_t *) char_r, 1);
 
+
+#define next_char_prog_a() \
+    back(); back();   swap();  drop();  plus();    emit();
+
+#define reload_prog_a() \
+    drop(); back();    back(); back(); \
+    swap(); drop();    swap(); drop();
+
+#define print_reloaded_buffer_prog_a() \
+    for (int i = 7; i > 0; i--) { \
+        emit(); \
+    } \
+    drop(); \
+    cr(); cr();
+
+// expects 1 2 3 4 5 6 7 and some value - 64 here -- on the stack
+// emits first four characters - program is capable to emit an
+// additional three chars (seven total).
+
+// Call next_char_prog_a() as required.
+#define fmac_prog_a()                                           \
+                                       rstack();   back();      \
+                      swap();           plus();    emit();      \
+    back(); back();   swap();           plus();    emit();      \
+    back(); back();   swap();  drop();  plus();    emit();      \
+    next_char_prog_a();            \
+    next_char_prog_a();            \
+    next_char_prog_a();            \
+    next_char_prog_a();            \
+    cr(); \
+    reload_prog_a();               \
+    print_reloaded_buffer_prog_a() \
+    print_reloaded_buffer_prog_a() \
+    print_reloaded_buffer_prog_a()
+
+/*
+
+\ nice working program - without responses
+
+  1 2 3 4   5 6 7  64        rstack back  .s
+               swap          plus   emit  .s
+  back back    swap          plus   emit  .s
+
+  back back    swap drop     plus   emit  .s
+  back back    swap drop     plus   emit  .s
+  back back    swap drop     plus   emit  .s
+  back back    swap drop     plus   emit  .s
+
+  back back    swap drop     plus   emit  .s
+
+  drop back    back back                  .s
+  swap drop    swap drop                  .s
+
+  emit A   emit B   emit C   emit D   cr
+  emit E   emit F   emit G   drop         .s
+
+  emit A   emit B   emit C   emit D   cr
+  emit E   emit F   emit G   drop         .s
+
+  emit A   emit B   emit C   emit D   cr
+  emit E   emit F   emit G   drop
+
+\ loads the stack with ints 1-7
+\ and a seed (offset) value for ASCII (65 - 1 = 64)
+\ 65 is ascii 'A'  (66 is 'B' and so on)
+
+\ prints them in sequence
+\ reuses them when it gets to the end
+
+\ wastes possibly the least amount of stack space
+\ possible, given the initial 'additive' method
+\ to populate the stack with a sequence of ASCII
+\ characters.
+
+*/
+
 /* end fmacros.h */
