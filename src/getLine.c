57871e1491879023476d1af1bbc92eb7f6ce2807
wa1tnr <wa1tnr@users.noreply.github.com>
2018-08-31 04:29:20 +0000

    New Branch Series - zKM_foo rather than xKM_foo
    See this file for details - getline.c aims to move
    beyond character-by-character interpretation, to
    account for complex expressions over a single (but
    somewhat longer) line of input, in a forth-like
    environment (a primitive, partial forth interpreter).
    
    On branch zKM_converser_d51-aa-
    
    	new file:   getLine.c

---
+++ src/getLine.c
@@ -0,0 +1,11 @@
+// Fri Aug 31 04:27:05 UTC 2018
+// On branch zKM_converser_d51-aa-
+
+/* getLine.c */
+
+// moving beyond interpreting each character as it is typed,
+// so that things such as '67 66 65 emit emit emit' produces
+// a contiguous string at the console, instead of interleaved
+// input and output, as it is now.  -31 August 2018 wa1tnr
+
+/* end, getLine.c */
