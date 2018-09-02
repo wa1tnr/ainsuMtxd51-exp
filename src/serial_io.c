4f600d60287af45e377740a9e34b86e2a5c487af
wa1tnr <wa1tnr@users.noreply.github.com>
2018-09-02 05:58:43 +0000

    prep to merge w/master On branch zKM_converser_d51-aa- 	modified:   ../src/serial_io.c

--- src/serial_io.c
+++ src/serial_io.c
@@ -1,4 +1,4 @@
-// Sun Sep  2 05:53:46 UTC 2018
+// Sun Sep  2 05:57:42 UTC 2018
 // On branch zKM_converser_d51-aa-
 
 /* wa1tnr - September, 2018 LGPL v2.1 */
@@ -26,19 +26,19 @@
 #include "dict_common.h"
 
 #define timeStamp(t,l) \
-    "Sun Sep  2 05:53:46 UTC 2018\r\n\r\n", 32
+    "Sun Sep  2 05:57:42 UTC 2018\r\n\r\n", 32
 
 /*
 #define branchStamp(b,l) \
   "zKM_converser_d51-aa-    ", 25
 */
 
-// current target branch:
-#define branchStamp(b,l) "KM_converser_d51         ", 25
-
 /*
-#define branchStamp(b,l) "master                   ", 25
+#define branchStamp(b,l) "KM_converser_d51         ", 25
 */
+
+// current target branch:
+#define branchStamp(b,l) "master                   ", 25
 
 struct io_descriptor *io;
 
